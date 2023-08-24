f9df70fec2fbaa601d35a1fa263d5a29fd977fe7
Elaine Ferreira <elaineferreira.dev@gmail.com>
2023-08-23 21:20:58 -0300

    fix: tool installation
    Fix the installation of Filezilla and standardize the
    installation of other tools.

--- minimal-gnome.sh
+++ minimal-gnome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Olá, você está preste a instalar o Gnome Mínimo"
-echo "Deseja continuar? (s/n)"
+echo "Deseja continuar? [S/n]"
 read install
 if [ "$install" == "s" -o "$install" == "S" -o "$install" == "" ]; then
 echo "##### Atualizando seu sistema #####"
@@ -18,7 +18,7 @@ echo "--- Instalação da Interface Gráfica Gnome Finalizada ---"
 
 echo "##### Instalação dos pacotes essenciais #####"
 
-apt install --no-install-recommends gdm3 nautilus tilix gnome-tweaks gnome-control-center network-manager gnome-software pulseaudio shotwell vlc ffmpegthumbnailer libgdk-pixbuf2.0-bin ntfs-3g sudo -y
+apt install --no-install-recommends gdm3 nautilus tilix gnome-tweaks gnome-control-center network-manager gnome-software pulseaudio shotwell vlc gnome-sushi ffmpegthumbnailer libgdk-pixbuf2.0-bin ntfs-3g sudo -y
 
 echo "--- Instalação dos pacotes essenciais Finalizada ---"
  
@@ -45,30 +45,19 @@ Para instalar os dois digite           3
 Para não instalar nenhum digite        0"
 read browser
 if [ "$browser" == 1 ]; then
-  echo "##### Instalando wget #####"
-
   apt install wget -y
-
-  echo "##### Baixando o Google Chrome #####"
-
-  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
 
   echo "##### Instalando o Google Chrome #####"
 
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
   dpkg -i chrome.deb
-
-  echo "##### Corrigindo dependências #####"
-
   apt install -f -y
-
   rm chrome.deb
-
-  echo "##### Instalação do Google Chrome Finalizada #####"
 
 elif [ "$browser" == 2 ]; then
   echo "##### Instalando o Firefox #####"
   
-  apt install firefox-esr -y
+  apt install --no-install-recommends firefox-esr -y
 
 elif [ "$browser" == 3 ]; then
   echo "##### Instalando Firefox e Google Chrome #####"
@@ -86,54 +75,49 @@ fi
 
 echo "##### Configurar ambiente de desenvolvimento #####"
 
-echo "Deseja instalar o Visual Studio Code? (s/n)"
+echo "Deseja instalar o Visual Studio Code? [S/n]"
 read vscode
 if [ "$vscode" == "s" -o "$vscode" == "S" -o "$vscode" == "" ]; then
-  echo "##### Baixando o Visual Studio Code #####"
-
-  wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb
-
   echo "##### Instalando o Visual Studio Code #####"
 
+  wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb
   dpkg -i vscode.deb
-
-  echo "##### Corrigindo dependências #####"
-
   apt install -f -y
-
   rm vscode.deb
-
-  echo "##### Instalação do Visual Studio Code Finalizada #####"
 fi
 
-echo "Deseja instalar o Filezilla? (s/n)"
+echo "Deseja instalar o Filezilla? [S/n]"
 read filezilla
-if [ "$filezilla" == "s" -o "$vscode" == "S" -o "$vscode" == "" ]; then
- apt install filezilla
+if [ "$filezilla" == "s" -o "$filezilla" == "S" -o "$filezilla" == "" ]; then
+  echo "##### Instalando o Filezilla #####"
+  apt install --no-install-recommends filezilla -y
 fi
 
-echo "Deseja instalar o Nodejs? (s/n)"
+echo "Deseja instalar o Nodejs? [S/n]"
 read nodejs
 if [ "$nodejs" == "s" -o "$nodejs" == "S" -o "$nodejs" == "" ]; then
-  apt install nodejs
+  echo "##### Instalando o Nodejs #####"
+  apt install --no-install-recommends nodejs -y
 fi
 
 echo "##### Instalar ferramentas de manipulação de imagens #####"
 
-echo "Deseja instalar o Inkscape? (s/n)"
+echo "Deseja instalar o Inkscape? [S/n]"
 read inkscape
 if [ "$inkscape" == "s" -o "$inkscape" == "S" -o "$inkscape" == "" ]; then
-  apt install inkscape
+  echo "##### Instalando o Inkscape #####"
+  apt install --no-install-recommends inkscape -y
 fi
 
-echo "Deseja instalar o Gimp? (s/n)"
+echo "Deseja instalar o Gimp? [S/n]"
 read gimp
 if [ "$gimp" == "s" -o "$gimp" == "S" -o "$gimp" == "" ]; then
-  apt install gimp
+  echo "##### Instalando o Gimp #####"
+  apt install --no-install-recommends gimp -y
 fi
 
 user=`users`
-echo "Deseja adicionar o usuário $user ao grupo sudo? (s/n)"
+echo "Deseja adicionar o usuário $user ao grupo sudo? [S/n]"
 read addusersudo
 if [ "$addusersudo" == "s" -o "$addusersudo" == "S" -o "$addusersudo" == "" ]; then
 usermod -aG sudo $user
@@ -143,7 +127,7 @@ fi
 echo "****** Parabéns!!******
       Seu Gnome Mínimo está instalado.
 Para desfrutar da sua nova instalação você precisa reiniciar o sistema."
-echo "Reiniciar sistema agora? (s/n)"
+echo "Reiniciar sistema agora? [S/n]"
 read restart
 if [ "$restart" == "s" -o "$restart" == "S" -o "$restart" == "" ]; then
   systemctl reboot
@@ -154,4 +138,3 @@ else
 echo "Instalação abortada!"
 
 fi
-
